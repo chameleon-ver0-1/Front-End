e8c4a32ebb12b11a30cc8a9640ec1fd5875213ac
jihu02 <jihu02@naver.com>
2019-03-14 15:53:20 +0900

    SignIn / SignOut Design

--- src/components/independent/auth/signin/SignIn.js
+++ src/components/independent/auth/signin/SignIn.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react'
+// import styled from 'styled-components'
+import './signin.style.css'
 
 class SignIn extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  state = {
+    checked: false
+  };
+
+  submitLogin(e) {}
+
+  handleChange = (e) => {
+    const { target: { checked } } = e;
+    this.setState({ checked });
+  }
+
   render() {
     return (
-      <h2>Hello Yoonyoung</h2>
+      <div className="inner-container">
+        <div className="header">
+          로그인
+        </div>
+        <div className="box-container">
 
+          <div className="input-group">
+            <label htmlFor="username">이메일</label>
+            <br/>
+            <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="이메일"/>
+          </div>
+          <br/>
+          <div className="input-group">
+            <label htmlFor="password">비밀번호</label>
+            <br/>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="비밀번호"/>
+          </div>
+          <br/>
 
-    )
+          <div>
+            <input onChange={this.handleChange} id={this.id} type="checkbox" checked={this.state.Checked} />
+            <label htmlFor={this.id}>로그인 상태 유지</label>
+
+            <input onChange={this.handleChange} id={this.id} type="checkbox" checked={this.state.Checked} />
+            <label htmlFor={this.id}>이메일 기억하기</label>
+          </div>
+
+          <br/>
+
+          <button
+            type="button"
+            className="login-btn"
+            onClick={this.submitLogin.bind(this)}>로그인</button>
+
+          <button
+           type="button"
+           className="google-btn"
+           onClick={this.submitLogin.bind(this)}>Google 로그인</button>
+
+           <button
+           type="button"
+           className="kakao-btn"
+           onClick={this.submitLogin.bind(this)}>카카오 로그인</button>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/independent/auth/signup/SignUp.js
+++ src/components/independent/auth/signup/SignUp.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react'
+import './signup.style.css'
 
 class SignUp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  submitRegister(e) {}
+
   render() {
     return (
-      <div>HAHAHA
+      <div className="inner-container">
+        <div className="header">
+          회원가입
+        </div>
+        <div className="box-container">
+
+          <div className="input-group">
+            <label htmlFor="username">이름</label>
+            <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="이름"/>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="email">이메일</label>
+            <input type="text" name="email" className="login-input" placeholder="이메일"/>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="비밀번호"/>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="company">회사 및 부서</label>
+            <br/>
+            <input
+              type="text"
+              name="company"
+              className="login-input2"
+              placeholder="회사명"/>
+
+              <input
+              type="text"
+              name="company"
+              className="login-input2"
+              placeholder="부서명"/>
+          </div>
+
+          <br/>
+          <button
+            type="button"
+            className="login-btn"
+            onClick={this.submitRegister.bind(this)}>가입하기</button>
+        </div>
       </div>
-    )
+    );
   }
 }
 export default SignUp
