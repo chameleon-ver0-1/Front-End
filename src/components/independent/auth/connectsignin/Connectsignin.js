63bc596efbd987d9d7ce2b1b03eb5111e8489a95
jihu02 <jihu02@naver.com>
2019-03-17 13:43:51 +0900

    create connectsignin

---
+++ src/components/independent/auth/connectsignin/Connectsignin.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './connectsignin.style.css'
+
+class Connectsignin extends Component{
+  render() {
+    return(
+      <div className="inner-container">
+        <div className="header">
+          연동 계정 로그인
+        </div>
+
+        <div className="box-container">
+
+        </div>
+      </div>
+    );
+  }
+}
