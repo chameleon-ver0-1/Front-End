a2278742e2e125b0f6f5ebedf906f19462b4e312
조윤영 <yoon980208@gmail.com>
2019-03-18 15:43:30 +0900

    edit signIn btn route

--- src/components/independent/auth/signin/SignIn.js
+++ src/components/independent/auth/signin/SignIn.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import styled from 'styled-components'
-import { Link } from 'react-router-dom';
-import './signin.style.css'
+import { Link } from "react-router-dom";
+import "./signin.style.css";
 
 class SignIn extends Component {
-
   constructor(props) {
     super(props);
     this.state = {};
@@ -14,86 +13,99 @@ class SignIn extends Component {
     checked: false
   };
 
-  submitLogin(e) { }
+  submitLogin(e) {}
 
-  handleChange = (e) => {
-    const { target: { checked } } = e;
+  handleChange = e => {
+    const {
+      target: { checked }
+    } = e;
     this.setState({ checked });
-  }
+  };
 
   render() {
     return (
-
       <div className="inner-container">
-
         <div className="box-container">
-          <div className="header">
-            로그인
-          </div>
+          <div className="header">로그인</div>
           <div className="input-group">
             {/* <label className="label" htmlFor="username">이메일</label> */}
-            <div className="label">
-              아이디
-            </div>
+            <div className="label">아이디</div>
 
             <input
               type="text"
               name="username"
               className="login-input"
-              placeholder="이메일 형식의 아이디를 입력해주세요" />
+              placeholder="이메일 형식의 아이디를 입력해주세요"
+            />
           </div>
 
           <div className="input-group">
             {/* <label className="label" htmlFor="password">비밀번호</label> */}
-            <div className="label">
-              비밀번호
-            </div>
+            <div className="label">비밀번호</div>
 
             <input
               type="password"
               name="password"
               className="login-input"
-              placeholder="비밀번호를 입력해주세요" />
+              placeholder="비밀번호를 입력해주세요"
+            />
           </div>
 
           <div className="checks">
-            <input onChange={this.handleChange} id={this.id} type="checkbox" checked={this.state.Checked} />
-            <label className="check" htmlFor={this.id}>로그인 상태 유지</label>
+            <input
+              onChange={this.handleChange}
+              id={this.id}
+              type="checkbox"
+              checked={this.state.Checked}
+            />
+            <label className="check" htmlFor={this.id}>
+              로그인 상태 유지
+            </label>
 
-            <input onChange={this.handleChange} id={this.id} type="checkbox" checked={this.state.Checked} />
-            <label className="check" htmlFor={this.id}>이메일 기억하기</label>
+            <input
+              onChange={this.handleChange}
+              id={this.id}
+              type="checkbox"
+              checked={this.state.Checked}
+            />
+            <label className="check" htmlFor={this.id}>
+              이메일 기억하기
+            </label>
           </div>
 
-
-
-          <Link to="/issue">
-            <button className="login-btn">
-              로그인
-            </button>
+          <Link to="/home/issue">
+            <button className="login-btn">로그인</button>
           </Link>
 
           <div className="buttons">
             <button
               type="button"
               className="google-btn"
-              onClick={this.submitLogin.bind(this)}>Google 로그인</button>
+              onClick={this.submitLogin.bind(this)}
+            >
+              Google 로그인
+            </button>
 
             <button
               type="button"
               className="kakao-btn"
-              onClick={this.submitLogin.bind(this)}>카카오 로그인</button>
-
+              onClick={this.submitLogin.bind(this)}
+            >
+              카카오 로그인
+            </button>
           </div>
 
           <button
             type="button"
             className="forget-btn"
-            onClick={this.submitLogin.bind(this)}>아이디/비밀번호를 잊으셨나요?</button>
-
+            onClick={this.submitLogin.bind(this)}
+          >
+            아이디/비밀번호를 잊으셨나요?
+          </button>
         </div>
       </div>
     );
   }
 }
 
-export default SignIn
+export default SignIn;
