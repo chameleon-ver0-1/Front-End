a753fb9dd90dd1ea14c7a6143af667053ce783fb
jihu02 <jihu02@naver.com>
2019-03-22 22:03:13 +0900

    table title & conference container

--- src/components/independent/conferenceRoom/ConferenceRoom.js
+++ src/components/independent/conferenceRoom/ConferenceRoom.js
@@ -1,8 +1,45 @@
 import React, { Component } from "react";
+import './conference.style.css';
 
 export class ConferenceRoom extends Component {
+
+  state = {
+    boards: [
+      {
+        roomtitle: '4월 간행물 표지 초안',
+        roommaker: '권주희 Kwon ju Headers',
+        roomtime: '2019.03.21',
+        roompeople: '5/5'
+      }
+    ]
+  }
   render() {
-    return <div />;
+    const { boards } = this.state;
+    return (
+      <div className="conferenceroom_container">
+        <text className="conferenceroom_text">
+          현재 진행중인 회의</text>
+
+        <button className="add_conference">+회의 개설하기</button>
+        <table className="conferenceroom_table" >
+          <tr className="table_head">
+            <th>방제목</th>
+            <th>개설자</th>
+            <th>회의시간</th>
+            <th>참여자</th>
+          </tr>
+
+          <tr>
+            <td>안녕</td>
+            <td>안지후</td>
+            <td>12시</td>
+            <td>5/5</td>
+          </tr>
+
+        </table>
+
+      </div>
+    );
   }
 }
 
