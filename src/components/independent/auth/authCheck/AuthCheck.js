72f4419ed01d52ac480fbd3bf1c2f67fc366008a
jihu02 <jihu02@naver.com>
2019-03-25 16:45:40 +0900

    authCheck design except one input repeat

--- src/components/independent/auth/authCheck/AuthCheck.js
+++ src/components/independent/auth/authCheck/AuthCheck.js
@@ -3,22 +3,30 @@ import { Link } from "react-router-dom";
 import "./authcheck.style.css";
 
 export class AuthCheck extends Component {
+
+  // hh = (gb) => {
+  //   var data = document.getElementById("g1");
+  //   if (data.)
+  // };
+
   render() {
     return (
       <div className="authCheck_container">
 
         <text className="authCheck_text">인증 번호를 입력해주세요</text>
 
-        <text className="authCheck_texts">이메일로 전송된 인증번호를 입력해주세요</text>
-        <br />
-        <text className="authCheck_texts">인증이 완료되면, 바로 카멜레On을 이용할 수 있습니다</text>
+        <div className="authCheck_texts_div">
+          <text className="authCheck_texts">이메일로 전송된 인증번호를 입력해주세요</text>
+          <br />
+          <text className="authCheck_texts">인증이 완료되면, 바로 카멜레On을 이용할 수 있습니다</text>
+        </div>
 
         <div className="authCheck_num_div">
-          <input className="authCheck_num"></input>
-          <input className="authCheck_num"></input>
-          <input className="authCheck_num"></input>
-          <input className="authCheck_num"></input>
-          <input className="authCheck_num"></input>
+          <input className="authCheck_num" id="g1" onKeyUp="hh(1);"></input>
+          <input className="authCheck_num" id="g2"></input>
+          <input className="authCheck_num" id="g3"></input>
+          <input className="authCheck_num" id="g4"></input>
+          <input className="authCheck_num" id="g5"></input>
         </div>
 
         {/* <button className="authCheck_reAuth">인증번호 재전송</button> */}
