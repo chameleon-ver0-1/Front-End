95a6a65d6ca2232b6ba5396b999584e2b83b1a55
조윤영 <yoon980208@gmail.com>
2019-04-08 16:40:34 +0900

    i finally did it!!!!!!!_show display in react

--- src/components/independent/webRTCRoom/UserArea.jsx
+++ src/components/independent/webRTCRoom/UserArea.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 
-import MediaHandler from "./MediaHandler";
-
-const { APP_KEY } = process.env;
+const rtcmulticonnection = require("rtcmulticonnection");
 
 export class UserArea extends Component {
+  componentWillMount() {
+    const script = document.createElement("script");
+
+    script.src = "https://cdn.webrtc-experiment.com/RTCMultiConnection.js";
+    script.src = "https://cdn.webrtc-experiment.com/conversation.js";
+    script.src =
+      "https://rtcmulticonnection.herokuapp.com/dist/RTCMultiConnection.min.js";
+    script.src =
+      "https://rtcmulticonnection.herokuapp.com/socket.io/socket.io.js";
+
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
+    var connection = new window.RTCMultiConnection();
+
+    // this line is VERY_important
+    connection.socketURL = "https://rtcmulticonnection.herokuapp.com:443/";
+
+    // if you want audio+video conferencing
+    connection.session = {
+      audio: true,
+      video: true
+    };
+
+    connection.openOrJoin("your-room-id");
     return (
       <div class="webrtc-container">
         <div>hi</div>
-        
-
+        <button>Join button</button>
+        <hr />
       </div>
     );
   }
