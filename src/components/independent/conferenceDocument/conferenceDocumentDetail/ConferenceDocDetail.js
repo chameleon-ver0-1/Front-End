b76ae4695b712f08fd1b693c52b3886513c14f63
jihu02@naver.com <jihu02@naver.com>
2019-05-20 21:41:11 +0900

    document display + wordcloud

--- src/components/independent/conferenceDocument/conferenceDocumentDetail/ConferenceDocDetail.js
+++ src/components/independent/conferenceDocument/conferenceDocumentDetail/ConferenceDocDetail.js
@@ -1,6 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import WordCloud from 'wordcloud';
+import './conferencedocdetail.style.css';
 
 class ConferenceDocDetail extends Component {
+
+  componentDidMount() {
+    WordCloud(this.refs['my-canvas'],
+      {
+        list: [['카멜레온', 15], ['컴퓨터공학과', 13], ['졸프', 10], ['화상회의', 8], ['회의록', 4],
+        ['노트북', 10], ['wordcloud', 7]],
+
+        weightFactor: 2,
+        fontWeight: 5,
+        color: function (word, weight) {
+          if (weight < 5) {
+            return '#c0c0c0';
+          }
+          else if (weight >= 5 && weight < 10) {
+            return '#696969';
+          }
+          else if (weight >= 10 && weight < 15) {
+            return '#202020';
+          }
+          else {
+            return '#34c88a';
+          }
+        },
+        // rotateRatio: 0.5,
+        // rotationSteps: 2,
+        backgroundColor: '#fafafa',
+        gridSize: 18,
+        hover: window.drawBox,
+        click: function (item) {
+          alert(item[0] + ': ' + item[1]);
+        }
+      });
+  }
+
   render() {
     return (
       <div>
@@ -11,6 +47,34 @@ class ConferenceDocDetail extends Component {
             <button>전체본</button>
             <button>요약본</button>
           </div>
+        </div>
+
+        <div className="wordclouddiv">
+          <div>키워드맵</div>
+          <div className="canvas-div">
+            <canvas ref="my-canvas"></canvas>
+          </div>
+        </div>
+
+        <div className="div1">
+          <div>주제1</div>
+          <div>요약 : 주제1에 대한 요약이 이곳에 들어갑니다.</div>
+          <div>요약 : 주제1에 대한 요약이 이곳에 들어갑니다.</div>
+          <div>요약 : 주제1에 대한 요약이 이곳에 들어갑니다.</div>
+        </div>
+
+        <div className="div2">
+          <div>주제2</div>
+          <div>요약 : 주제2에 대한 요약이 이곳에 들어갑니다.</div>
+          <div>요약 : 주제2에 대한 요약이 이곳에 들어갑니다.</div>
+          <div>요약 : 주제2에 대한 요약이 이곳에 들어갑니다.</div>
+        </div>
+
+        <div className="div3">
+          <div>주제1</div>
+          <div>요약 : 주제3에 대한 요약이 이곳에 들어갑니다.</div>
+          <div>요약 : 주제3에 대한 요약이 이곳에 들어갑니다.</div>
+          <div>요약 : 주제3에 대한 요약이 이곳에 들어갑니다.</div>
         </div>
 
       </div>
