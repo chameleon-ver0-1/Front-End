83acf69e5296a4d457758095efff77609f56876b
jihu02@naver.com <jihu02@naver.com>
2019-05-29 13:35:12 +0900

    css design done & have to put date-clock, tag input

--- src/components/independent/conferenceRoom/makeRoom/MakeRoom.js
+++ src/components/independent/conferenceRoom/makeRoom/MakeRoom.js
@@ -6,9 +6,34 @@ class MakeRoom extends Component {
   render() {
     const { open, title, onCloseModal } = this.props;
     return (
-      <Modal open={open} onClose={onCloseModal} center>
-        <div>
-          <div>{title}</div>
+      <Modal open={open} onClose={onCloseModal} center >
+        <div className="makeroomdiv">
+          <div className="roomtitle">{title}</div>
+          <div className="row-div">
+            <div className="roomtitle2">방제목</div>
+            <input className="roominput"></input>
+          </div>
+
+          <div className="row-div">
+            <div className="roomtitle2">메인 토픽</div>
+            <input className="roominput"></input>
+          </div>
+
+          <div className="row-div">
+            <div className="roomtitle2">시작 시간</div>
+            <div></div>
+          </div>
+
+          <div className="row-div">
+            <div className="roomtitle2">참여자</div>
+            <input className="roominput"></input>
+          </div>
+
+          <div className="row-div2">
+            <button className="makebutton">개설</button>
+            <button className="cancelbutton" onClick={onCloseModal}>취소</button>
+          </div>
+
         </div>
       </Modal>
     )
