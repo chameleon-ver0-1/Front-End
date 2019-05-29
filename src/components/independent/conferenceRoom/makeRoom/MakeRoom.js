f6e91a0e56039e4e4778e3505e871414eda1fd16
jihu02@naver.com <jihu02@naver.com>
2019-05-29 16:09:34 +0900

    Tag (except max 3)

--- src/components/independent/conferenceRoom/makeRoom/MakeRoom.js
+++ src/components/independent/conferenceRoom/makeRoom/MakeRoom.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react'
 import './makeroom.style.css'
 import Modal from "react-responsive-modal";
+import { render } from 'react-dom';
+import { TAG } from './tag';
+import { WithContext as ReactTags } from 'react-tag-input';
+
+const suggestions = TAG.map((tag) => {
+  return {
+    id: tag,
+    text: tag
+  }
+})
+
+const KeyCodes = {
+  comma: 188,
+  enter: 13,
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
 class MakeRoom extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tags: [{ id: 'Web', text: 'Web' }],
+      suggestions: suggestions,
+    };
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleAddition = this.handleAddition.bind(this);
+    this.handleDrag = this.handleDrag.bind(this);
+    this.handleTagClick = this.handleTagClick.bind(this);
+  }
+
+  handleDelete(i) {
+    const { tags } = this.state;
+    this.setState({
+      tags: tags.filter((tag, index) => index !== i),
+    });
+  }
+
+  handleAddition(tag) {
+    this.setState(state => ({ tags: [...state.tags, tag] }));
+  }
+
+  handleDrag(tag, currPos, newPos) {
+    const tags = [...this.state.tags];
+    const newTags = tags.slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+
+    // re-render
+    this.setState({ tags: newTags });
+  }
+
+  handleTagClick(index) {
+    console.log('The tag at index ' + index + ' was clicked');
+  }
+
   render() {
     const { open, title, onCloseModal } = this.props;
+    const { tags, suggestions } = this.state;
+
     return (
       <Modal open={open} onClose={onCloseModal} center >
         <div className="makeroomdiv">
@@ -16,7 +74,18 @@ class MakeRoom extends Component {
 
           <div className="row-div">
             <div className="roomtitle2">메인 토픽</div>
-            <input className="roominput"></input>
+            <div>
+              <ReactTags
+                inputFieldPosition="top"
+                tags={tags}
+                suggestions={suggestions}
+                delimiters={delimiters}
+                handleDelete={this.handleDelete}
+                handleAddition={this.handleAddition}
+                handleDrag={this.handleDrag}
+                handleTagClick={this.handleTagClick}
+              />
+            </div>
           </div>
 
           <div className="row-div">
