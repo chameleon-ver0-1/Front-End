eca5a11c0ae7afef73195abb274cd07317466d67
jihu02@naver.com <jihu02@naver.com>
2019-06-20 23:59:39 +0900

    date time ing

--- src/components/independent/conferenceRoom/makeRoom/MakeRoom.js
+++ src/components/independent/conferenceRoom/makeRoom/MakeRoom.js
@@ -5,8 +5,6 @@ import { render } from 'react-dom';
 import { TAG } from './tag';
 import { WithContext as ReactTags } from 'react-tag-input';
 import { Link } from "react-router-dom";
-// import { makeStyles } from '@material-ui/core/styles';
-// import TextField from '@material-ui/core/TextField';
 
 const suggestions = TAG.map((tag) => {
   return {
@@ -107,17 +105,7 @@ class MakeRoom extends Component {
           <div className="row-div">
             <div className="roomtitle2">시작 시간</div>
             <div>
-              {/* <form className={classes.container} noValidate>
-                <TextField
-                  id="datetime-local"
-                  type="datetime-local"
-                  defaultValue="2017-05-24T10:30"
-                  className={classes.textField}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                />
-              </form> */}
+
             </div>
           </div>
 
