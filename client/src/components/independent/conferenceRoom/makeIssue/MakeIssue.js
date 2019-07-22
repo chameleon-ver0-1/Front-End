bd4c9765e06a4cb311d477342738a81d6fecc377
jihu02@naver.com <jihu02@naver.com>
2019-07-22 19:52:35 +0900

    [UPDATE] Tab 디자인

--- client/src/components/independent/conferenceRoom/makeIssue/MakeIssue.js
+++ client/src/components/independent/conferenceRoom/makeIssue/MakeIssue.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Modal from "react-responsive-modal";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "./makeissue.style.css";
 
 export class MakeIssue extends Component {
   render() {
@@ -14,6 +16,35 @@ export class MakeIssue extends Component {
       >
         <div className="makeroomdiv">
           <div className="roomtitle">{title}</div>
+          <Tabs>
+            <TabList>
+              <Tab>TODO</Tab>
+              <Tab>DOING</Tab>
+              <Tab>DONE</Tab>
+            </TabList>
+
+            <TabPanel>
+              <ul className="issue-list">
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+              </ul>
+            </TabPanel>
+            <TabPanel>
+              <ul className="issue-list">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </TabPanel>
+            <TabPanel>
+              <ul className="issue-list">
+                <li>x</li>
+                <li>y</li>
+                <li>z</li>
+              </ul>
+            </TabPanel>
+          </Tabs>
         </div>
       </Modal>
     );
