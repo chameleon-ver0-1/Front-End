a010612fef40e761e0b2a6fa57fd5b7ecb9b6bbb
조윤영 <yoon980208@gmail.com>
2019-09-03 01:15:44 +0900

    [UPDATE]프로젝트 리스트 네트워킹 완성

--- client/src/components/independent/auth/project2/ProjectList.js
+++ client/src/components/independent/auth/project2/ProjectList.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import "./projectlist.style.css";
 import { Link } from "react-router-dom";
 import * as service from "../../../../services/ProjectService";
+import {
+  ProjectListContainer,
+  ProjectListHeader,
+  ProjectListItemContainer,
+  ProjectListItemBtn
+} from "./projectlist.style";
 
 export class ProjectList extends Component {
+  state = { projectLists: [] };
   componentDidMount() {
     service.getProjectList().then(
-      res => {},
+      res => {
+        this.setState({ projectLists: res.data.data });
+      },
       err => {
         console.log("프로젝트 리스트 가져오기 실패");
       }
@@ -14,27 +22,24 @@ export class ProjectList extends Component {
   }
   render() {
     return (
-      <div className="inner-container3">
-        <div className="header3">참여 중인 프로젝트</div>
+      <ProjectListContainer>
+        <ProjectListHeader>참여 중인 프로젝트</ProjectListHeader>
         <div className="p-explanation">현재 참여 중인 프로젝트 입니다.</div>
         <div className="p-explanation">
           접근을 원하시는 프로젝트를 선택하세요.
         </div>
+        <ProjectListItemContainer>
+          {Object.keys(this.state.projectLists).map(projectListId => {
+            const projectlist = this.state.projectLists[projectListId];
 
-        <div className="pl-btn-div">
-          <Link to="/home/issue">
-            <button className="project-btn">Project1</button>
-          </Link>
-
-          <Link to="/home/issue">
-            <button className="project-btn">Past Project1</button>
-          </Link>
-
-          <Link to="/home/issue">
-            <button className="project-btn">Past Project2</button>
-          </Link>
-        </div>
-      </div>
+            return (
+              <ProjectListItemBtn to="/home/issue">
+                {projectlist.projectName}
+              </ProjectListItemBtn>
+            );
+          })}
+        </ProjectListItemContainer>
+      </ProjectListContainer>
     );
   }
 }
