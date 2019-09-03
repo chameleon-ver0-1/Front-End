1cfc92db1c26fe804e8b5d0a5fa3675fedb3a78c
jihu02@naver.com <jihu02@naver.com>
2019-09-03 21:33:15 +0900

    [UPDATE] 프로젝트개설 Auth 추가

--- client/src/services/ProjectAddService.js
+++ client/src/services/ProjectAddService.js
@@ -8,7 +8,9 @@ import { SET_CURRENT_USER } from "../helpers/types";
 const projectAddAxios = axios.create({
   baseURL: "https://a.chameleon4switch.cf/api",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    Authorization:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJDaGFtZWxlb24iLCJzdWIiOiJ0ZXN0MDFAZ21haWwuY29tIiwiaWF0IjoxNTY2ODMyODI4NzQyLCJleHAiOjE1NjY5MTkyMjg3NDJ9.JbT0WUKxRuTGfDVwSzSZZBMaf_GGv6r5YQndXma6Zro"
   },
   timeout: 5000
 });
