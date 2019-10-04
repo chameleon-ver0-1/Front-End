a0a841e6b3bc1f90c388e48b6298144051fba916
jihu02@naver.com <jihu02@naver.com>
2019-10-04 21:51:13 +0900

    [UPDATE] 프로젝트 이름 넘기기

--- client/src/components/independent/auth/project/ProjectAdd.js
+++ client/src/components/independent/auth/project/ProjectAdd.js
@@ -31,6 +31,7 @@ export class ProjectAdd extends Component {
         res => {
           console.log(res.data.data._id);
           if (res.data.message === "프로젝트 생성 성공") {
+            localStorage.setItem("projectName", res.data.data.name);
             localStorage.setItem("projectId", res.data.data._id);
             this.props.history.push(`/home/issue/${res.data.data._id}`);
             console.log("프로젝트 개설 성공");
