ebc5b4474fd911f675573680c7436affd477ea66
jihu02@naver.com <jihu02@naver.com>
2019-10-25 00:10:50 +0900

    [UPDATE] PDF 화면 깨짐 수정

--- client/src/components/independent/conferenceDocument/conferenceDocumentDetail/PrintButton.js
+++ client/src/components/independent/conferenceDocument/conferenceDocumentDetail/PrintButton.js
@@ -4,23 +4,6 @@ import jsPDF from "jspdf";
 import download_off from "../../../../assets/doc/download_off.png";
 import "./conferencedocdetail.style.css";
 
-const pxToMm = px => {
-  return Math.floor(px / document.getElementById("myMm").offsetHeight);
-};
-
-const mmToPx = mm => {
-  return document.getElementById("myMm").offsetHeight * mm;
-};
-
-const range = (start, end) => {
-  return Array(end - start)
-    .join(0)
-    .split(0)
-    .map(function(val, id) {
-      return id + start;
-    });
-};
-
 const PrintButton = ({ id, label }) => (
   <div className="tc mb4 mt2">
     {/*
@@ -33,38 +16,13 @@ const PrintButton = ({ id, label }) => (
       className="sum-div"
       onClick={() => {
         const input = document.getElementById(id);
-        const inputHeightMm = pxToMm(input.offsetHeight);
-        const a4WidthMm = 250;
-        const a4HeightMm = 297;
-        const a4HeightPx = mmToPx(a4HeightMm);
-        const numPages =
-          inputHeightMm <= a4HeightMm
-            ? 1
-            : Math.floor(inputHeightMm / a4HeightMm) + 1;
-        console.log({
-          input,
-          inputHeightMm,
-          a4HeightMm,
-          a4HeightPx,
-          numPages,
-          range: range(0, numPages),
-          comp: inputHeightMm <= a4HeightMm,
-          inputHeightPx: input.offsetHeight
-        });
 
         html2canvas(input).then(canvas => {
           const imgData = canvas.toDataURL("image/png");
 
-          // Document of a4WidthMm wide and inputHeightMm high
-          if (inputHeightMm > a4HeightMm) {
-            // elongated a4 (system print dialog will handle page breaks)
-            const pdf = new jsPDF("p", "mm", "a4");
-          } else {
-            // standard a4
-            const pdf = new jsPDF("p", "mm", "a4");
-          }
-          const pdf = new jsPDF();
-          pdf.addImage(imgData, "PNG", 0, 0);
+          const pdf = new jsPDF("p", "mm", [canvas.width, canvas.height]);
+
+          pdf.addImage(imgData, "PNG", 10, 3, imgData.width, imgData.height);
           pdf.save(`${id}.pdf`);
         });
       }}
